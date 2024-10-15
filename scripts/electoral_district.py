eb0b173fca185a6ce95cdd299d84e0e558091614
Sanjaya-Nuwan <77116442+Sanjaya-Nuwan@users.noreply.github.com>
2024-10-15 22:37:13 +0530

    feat/new-scripts-2024-election

---
+++ scripts/electoral_district.py
@@ -0,0 +1,258 @@
+lst = [
+    {
+        "name": "Western",
+        "districts": {
+            "Colombo": [
+                "Colombo-North",
+                "Colombo-Central",
+                "Borella",
+                "Colombo-East",
+                "Colombo-West",
+                "Dehiwala",
+                "Ratmalana",
+                "Kolonnawa",
+                "Kotte",
+                "Kaduwela",
+                "Avissawella",
+                "Homagama",
+                "Maharagama",
+                "Kesbewa",
+                "Moratuwa"
+            ],
+            "Gampaha": [
+                "Wattala",
+                "Negombo",
+                "Katana",
+                "Divulapitiya",
+                "Mirigama",
+                "Minuwangoda",
+                "Attanagalla",
+                "Gampaha",
+                "Ja-Ela",
+                "Mahara",
+                "Dompe",
+                "Biyagama",
+                "Kelaniya"
+            ],
+            "Kalutara": [
+                "Panadura",
+                "Bandaragama",
+                "Horana",
+                "Bulathsinhala",
+                "Matugama",
+                "Kalutara",
+                "Beruwala",
+                "Agalawatta",
+            ]
+        }
+    },
+    {
+        "name": "Southern",
+        "districts": {
+            "Matara": [
+                "Deniyaya",
+                "Hakmana",
+                "Akuressa",
+                "Kamburupitiya",
+                "Devinuwara",
+                "Matara",
+                "Weligama",
+            ],
+            "Hambantota": [
+                "Mulkirigala",
+                "Beliatta",
+                "Tangalle",
+                "Tissamaharama",
+            ],
+            "Galle": [
+                "Balapitiya",
+                "Ambalangoda",
+                "Karandeniya",
+                "Bentara-Elpitiya",
+                "Hiniduma",
+                "Baddegama",
+                "Ratgama",
+                "Galle",
+                "Akmeemana",
+                "Habaraduwa",
+            ]
+        }
+    },
+    {
+        "name": "Central",
+        "districts": {
+            "Matale": [
+                "Dambulla",
+                "Laggala",
+                "Matale",
+                "Rattota",
+            ],
+            "Mahanuwara": [
+                "Galagedara",
+                "Harispattuwa",
+                "Patha-Dumbara",
+                "Udu-Dumbara",
+                "Teldeniya",
+                "Kundasale",
+                "Hewaheta",
+                "Senkadagala",
+                "Mahanuwara",
+                "Yatinuwara",
+                "Udunuwara",
+                "Gampola",
+                "Nawalapitiya",
+            ],
+            "NuwaraEliya": [
+                "NEliya-Maskeliya",
+                "Kotmale",
+                "Hanguranketha",
+                "Walapane",
+            ]
+        }
+
+    },
+    {
+        "name": "Northern",
+        "districts": {
+            "Jaffna": [
+                "Kayts",
+                "Vaddukkoddai",
+                "Kankesanturai",
+                "Manipay",
+                "Kopay",
+                "Udupiddy",
+                "Point%20Pedro",
+                "Chavakachcheri",
+                "Nallur",
+                "Jaffna",
+                "Kilinochchi",
+            ],
+            "Vanni": [
+                "Mannar",
+                "Vavuniya",
+                "Mullaitivu"
+            ]
+        }
+
+    },
+    {
+        "name": "Eastern",
+        "districts": {
+            "Batticaloa": [
+                "Kalkudah",
+                "Batticaloa",
+                "Padiruppu",
+            ],
+            "Digamadulla": [
+                "Ampara",
+                "Sammanturai",
+                "Kalmunai",
+                "Potuvil",
+            ],
+            "Trincomalee": [
+                "Seruwila",
+                "Trincomalee",
+                "Mutur"
+            ]
+        }
+
+    },
+    {
+        "name": "North Western",
+        "districts": {
+            "Kurunegala": [
+                "Galgamuwa",
+                "Nikaweratiya",
+                "Yapahuwa",
+                "Hiriyala",
+                "Wariyapola",
+                "Panduwasnuwara",
+                "Bingiriya",
+                "Katugampola",
+                "Kuliyapitiya",
+                "Dambadeniya",
+                "Polgahawela",
+                "Kurunegala",
+                "Mawathagama",
+                "Dodangaslanda"
+            ],
+            "Puttalam": [
+                "Puttalam",
+                "Anamaduwa",
+                "Chillaw",
+                "Nattandiya",
+                "Wennappuwa",
+            ]
+        }
+
+    },
+    {
+        "name": "North Central",
+        "districts": {
+            "Anuradhapura": [
+                "Medawachchiya",
+                "Horowupotana",
+                "Anuradhapura-East",
+                "Anuradhapura-West",
+                "Kalawewa",
+                "Mihintale",
+                "Kekirawa",
+            ],
+            "Polonnaruwa": [
+                "Minneriya",
+                "Medirigiriya",
+                "Polonnaruwa",
+            ]
+        }
+    },
+    {
+        "name": "Uva",
+        "districts": {
+            "Badulla": [
+                "Mahiyangane",
+                "Wiyaluwa",
+                "Passara",
+                "Badulla",
+                "Hali_Ela",
+                "Uva-Paranagama",
+                "Welimada",
+                "Bandarawela",
+                "Haputale",
+            ],
+            "Moneragala": [
+                "Bibile",
+                "Moneragala",
+                "Wellawaya",
+            ]
+        }
+
+    },
+    {
+        "name": "Sabaragamuwa",
+        "districts": {
+            "Kegalle": [
+                "Dedigama",
+                "Galigamuwa",
+                "Kegalle",
+                "Rambukkana",
+                "Mawanella",
+                "Aranayake",
+                "Yatiyantota",
+                "Ruwanwella",
+                "Deraniyagala",
+
+            ],
+            "Ratnapura": [
+                "Eheliyagoda",
+                "Ratnapura",
+                "Pelmadulla",
+                "Balangoda",
+                "Rakwana",
+                "Nivitigala",
+                "Kalawana",
+                "Kolonna",
+            ]
+        }
+    }
+
+]
---
+++ scripts/postal_districts.py
@@ -0,0 +1,114 @@
+lst = [
+    {
+        "name": "Western",
+        "districts": {
+            "Colombo": [
+                "Postal-Votes-Col"
+            ],
+            "Gampaha": [
+                "Postal-Votes-Gampaha"
+            ],
+            "Kalutara": [
+                "Postal-Votes-Kalutara"
+            ]
+        }
+    },
+    {
+        "name": "Southern",
+        "districts": {
+            "Matara": [
+                "Postal-Votes-Matara"
+            ],
+            "Hambantota": [
+                "Postal-Votes-Htota"
+            ],
+            "Galle": [
+                "Postal-Votes-Galle"
+            ]
+        }
+    },
+    {
+        "name": "Central",
+        "districts": {
+            "Matale": [
+                "Postal-Votes-Matale"
+            ],
+            "Mahanuwara": [
+                "Postal-Votes-Mnuwara"
+            ],
+            "NuwaraEliya": [
+                "Postal-Votes-NEliya"
+            ]
+        }
+    },
+    {
+        "name": "Northern",
+        "districts": {
+            "Jaffna": [
+                "Postal-Votes-Jaffna"
+            ],
+            "Vanni": [
+                "Postal-Votes-Vanni"
+            ]
+        }
+    },
+    {
+        "name": "Eastern",
+        "districts": {
+            "Batticaloa": [
+                "Postal-Votes-Bcaloa"
+            ],
+            "Digamadulla": [
+                "Postal-Votes-Dmadulla"
+            ],
+            "Trincomalee": [
+                "Postal-Votes-Trinco"
+            ]
+        }
+    },
+    {
+        "name": "North Western",
+        "districts": {
+            "Kurunegala": [
+                "Postal-Votes-Knegala"
+            ],
+            "Puttalam": [
+                "Postal-Votes-Pulam"
+            ]
+        }
+    },
+    {
+        "name": "North Central",
+        "districts": {
+            "Anuradhapura": [
+                "Postal-Votes-Apura"
+            ],
+            "Polonnaruwa": [
+                "Postal-Votes-Pnaruwa"
+            ]
+        }
+    },
+    {
+        "name": "Uva",
+        "districts": {
+            "Badulla": [
+                "Postal-Votes-Badulla"
+            ],
+            "Moneragala": [
+                "Postal-Votes-Mgala"
+            ]
+        }
+    },
+    {
+        "name": "Sabaragamuwa",
+        "districts": {
+            "Kegalle": [
+                "Postal-Votes-Kegalle"
+            ],
+            "Ratnapura": [
+                "Postal-Votes-Rpura"
+            ]
+        }
+    }
+
+]
